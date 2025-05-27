114a467bff93c642b58431c8348e75777db68ed8
shoukatrai <raishoukat033@gmail.com>
2025-05-27 14:28:57 +0500

    get github profile completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './components/card'
 
 const App = () => {
-  const [userName, setUserName] = useState("")
+  const [userName, setUserName] = useState("shoukatrai")
   const [userProfile, setUserProfile] = useState("")
 
 
   const getProfile = async () => {
     const profile = await axios.get(`https://api.github.com/users/${userName}`)
+    if(!profile){
+      alert("User Not Found!")
+      return
+    }
     console.log("profile", profile)
     setUserProfile(profile)
   }
 
-const handleSubmit = (event)=>{
-  event.preventDefault()
-  getProfile()
-}
+  useEffect(() => {
+    getProfile()
+  }, [userName])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getProfile()
+  }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setUserName(e.target.value)} />
+    <div className='w-70% m-auto flex items-center justify-between flex-col gap-20 mt-3'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-3 text-center justify-center items-center '>
+        <h1 className='text-2xl font-bold'>Get GitHub Profile</h1>
+        <input 
+        type="text" 
+        onChange={e => setUserName(e.target.value)} 
+        className='text-sm p-3 border rounded'
+         placeholder='Enter your username' />
       </form>
 
-      <Card userProfile = {userProfile}/>
+      <Card userProfile={userProfile} />
     </div>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 
 const Card = ({userProfile}) => {
-    console.log("userProfile" , userProfile.data.login)
-    console.log("username" , userProfile.data.login)
-    console.log("location" , userProfile.data.location)
-    console.log("bio" , userProfile.data.bio)
-    console.log("follower" , userProfile.data.followers)
-    console.log("following" , userProfile.data.following)
-    console.log("imageurl" , userProfile.data.avatar_url)
-    console.log("name" , userProfile.data.name)
   return (
-    <div>
-      <h1>{  "abdhsdhsh"}</h1>
-      <img src= {null} alt="Git hub profile image" />
+    <div className='text-center flex flex-col gap-3 p-3 justify-center items-center bg-black text-white rounded-2xl' >
+      <h1 className='text-2xl'>Name: {userProfile?.data?.name || "shoukat"}</h1>
+      <h1>{userProfile?.data?.login || "abdhsdhsh"}</h1>
+      <img src={userProfile?.data?.avatar_url || ""} alt="Git hub profile image"  className='rounded-full sm:w-100% h-100% lg:w-100 lg:h-100'/>
+      <h2>{userProfile?.data?.bio || "web developer"}</h2>
+      <h2>{userProfile?.data?.location || "Karachi"}</h2>
+      <h2>follower: {userProfile?.data?.followers || "0"}</h2>
+      <h2>following: {userProfile?.data?.following || "0"}</h2>
     </div>
   )
 }
